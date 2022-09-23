ccc5e609707d6394d3f575ea7d7364080b5c26f9
Anmol Kumar <anmolshrivastav.08@gmail.com>
2022-09-23 17:16:10 +0530

    Created new contact ui

--- app/javascript/src/components/Dashboard/Contacts/constants.js
+++ app/javascript/src/components/Dashboard/Contacts/constants.js
@@ -1,3 +1,6 @@
+import { buildSelectOptions } from "utils";
+import * as yup from "yup";
+
 export const CONTACT_DETAILS_DATA = [
   {
     id: 1,
@@ -72,3 +75,28 @@ export const CONTACT_DETAILS_DATA = [
     createdAt: "Feb 15, 2021",
   },
 ];
+
+export const CONTACTS_FROM_INITIAL_VALUES = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  role: null,
+};
+export const ROLES = buildSelectOptions(["Admin", "Non Admin"]);
+
+export const CONTACTS_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  firstName: yup.string().required("First name is required."),
+  lastName: yup.string().required("Last name is required."),
+  email: yup
+    .string()
+    .required("Email address is required.")
+    .email("Invalid email address."),
+  role: yup
+    .object()
+    .nullable()
+    .shape({
+      label: yup.string().oneOf(ROLES.map(role => role.label)),
+      value: yup.string().oneOf(ROLES.map(role => role.value)),
+    })
+    .required("Role is required."),
+});
